f8bd3f9a708c6503f0c24c90159fcf4b4677a066
Kobe <terracotta711@gmail.com>
2017-05-18 23:50:32 +0800

    add LogoPage example

---
+++ LogoPageExample/app/src/main/java/kobe/logopageexample/MainActivity.java
@@ -0,0 +1,29 @@
+package kobe.logopageexample;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_page);
+        getSupportActionBar().hide();
+
+        //>> display the logo page for 3 secs
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                setContentView(R.layout.activity_main);
+                getSupportActionBar().show();
+            }
+        }.start();
+    }
+}
