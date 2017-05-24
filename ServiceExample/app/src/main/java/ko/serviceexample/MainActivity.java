2ebacbdf78bc4eef839a09803f23e2208f23928b
kobe <terracotta711@gmail.com>
2017-05-25 00:27:40 +0800

    add Service Example

---
+++ ServiceExample/app/src/main/java/ko/serviceexample/MainActivity.java
@@ -0,0 +1,26 @@
+package ko.serviceexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartServiceClick(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+    }
+
+    //>> should see the Log :  D/MyService: onDestroy
+    public void onStopServiceClick(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+    }
+}
---
+++ ServiceExample/app/src/main/java/ko/serviceexample/MyService.java
@@ -0,0 +1,43 @@
+package ko.serviceexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+//>> also need to add <service> to manifest
+public class MyService extends Service {
+    final String TAG = "MyService";
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    //>> entry point of stand-alone service
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        Log.d(TAG, "downloading....");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "download is completed");
+            }
+        }, 3000);
+
+        Log.d(TAG, "onStartCommand is going to finish");
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
