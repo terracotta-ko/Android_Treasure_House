1e215abba6849238bfee6f73ae929e6419369282
Kobe <terracotta711@gmail.com>
2019-09-12 22:02:30 +0100

    upgrade to androidx

--- Factory_Design_Pattern_Example/app/src/main/java/kobe/factory_design_pattern_example/AnimalFactory.java
+++ Factory_Design_Pattern_Example/app/src/main/java/kobe/factory_design_pattern_example/AnimalFactory.java
@@ -1,6 +1,6 @@
 package kobe.factory_design_pattern_example;
 
-import android.support.annotation.IntDef;
+import androidx.annotation.IntDef;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
