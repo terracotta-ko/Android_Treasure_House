19b4ad4fc04d3830530832d1869d5bd7560f1e5f
Kobe <terracotta711@gmail.com>
2019-09-12 21:32:44 +0100

    upgrade to androidx

--- RecyclerView_Swipe_Menu/app/src/main/java/kobe/recyclerview_swipe_menu/MainActivity.java
+++ RecyclerView_Swipe_Menu/app/src/main/java/kobe/recyclerview_swipe_menu/MainActivity.java
@@ -2,11 +2,11 @@ package kobe.recyclerview_swipe_menu;
 
 import android.graphics.Canvas;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
