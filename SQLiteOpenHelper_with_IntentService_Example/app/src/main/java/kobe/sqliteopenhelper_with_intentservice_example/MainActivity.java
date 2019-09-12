72b148f9192c100a905b5149cc9c4d0e15dd22a4
Kobe <terracotta711@gmail.com>
2019-09-12 22:38:29 +0100

    upgrade to androidx

--- SQLiteOpenHelper_with_IntentService_Example/app/src/main/java/kobe/sqliteopenhelper_with_intentservice_example/MainActivity.java
+++ SQLiteOpenHelper_with_IntentService_Example/app/src/main/java/kobe/sqliteopenhelper_with_intentservice_example/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
